232b5a3ece6c240d478e42aaf95e5ff48c183266
Thayan Alves <thayan_fsa10@hotmail.com>
2019-04-27 00:16:24 -0300

    projeto sendo criado

--- cadastro-projeto/criar-projeto.php
+++ cadastro-projeto/criar-projeto.php
@@ -1,147 +1,185 @@
+<?php
+
+session_start();
+if (!isset($_SESSION['login']) && !isset($_SESSION['senha'])):
+endif;
+
+if (isset($_POST['cadastrar'])) {
+    include('../classes/Conexao.class.php');
+    include('../classes/ProjetoDAO.class.php');
+
+    $cadastrar = new ProjetoDAO();
+
+    $codUsuario = $_SESSION['codUsuario'];
+    $nomeProjeto = trim(strip_tags($_POST['nomeProjeto']));
+    $nomeProfessor = trim(strip_tags($_POST['nomeProfessor']));
+    $objetivoProjeto = trim(strip_tags($_POST['objetivoProjeto']));
+    $resumoProjeto = trim(strip_tags($_POST['resumoProjeto']));
+    $cursoProjeto = trim(strip_tags($_POST['cursoProjeto']));
+    $turmaProjeto = trim(strip_tags($_POST['turmaProjeto']));
+
+    echo "$codUsuario";
+    echo "$nomeProjeto";
+    echo "$nomeProfessor";
+    echo "$objetivoProjeto";
+    echo "$resumoProjeto";
+    echo "$cursoProjeto";
+    echo "$turmaProjeto";
+
+
+//    if ($nomeProjeto != $nomeProfessor) {
+//        $insere = $cadastrarProjeto->cadastraProjeto($codUsuario, $nomeProjeto, $nomeProfessor, $objetivoProjeto, $resumoProjeto, $cursoProjeto, $turmaProjeto);
+//        // caso o usuario seja cadastrado, exibir mensagem de sucesso
+//        if ($insere == true) {
+//            header('location:index.php?successUser=cadastrado');
+//        }
+//    }
+
+    if (isset($codUsuario)) {
+        echo "Entro Aqui";
+        $consulta = $cadastrar->unicoProjeto($nomeProjeto);
+        echo "Passo Aqui";
+        if ($consulta == false) {
+            echo "Criar Erro aqui";
+            header('location:criar-projeto.php?repetido=senha');
+            // caso não haja login parecido, inclui métoro de inserção de dados no banco de dados
+        } else {
+              $insere = $cadastrar->cadastra($codUsuario, $nomeProjeto, $nomeProfessor, $objetivoProjeto, $resumoProjeto, $cursoProjeto, $turmaProjeto);
+            // caso o usuario seja cadastrado, exibir mensagem de sucesso
+            if ($insere == true) {
+                echo "Sucesso qui";
+                header('location:criar-projeto.php?successUser=cadastrado');
+            }
+        }
+
+    } else {
+        echo "Outro erro aqui";
+        header('location:criar-projeto.php?erro=senha');
+    }
+
+}
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="pt-br">
 
 <head>
     <meta charset="utf-8">
     <!-- Latest compiled and minified CSS -->
-  <link rel="stylesheet" href="assets/css/bootstrap.min.css">
-  <title>Cambury | Cadastro de Projeto</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <title>Cambury | Cadastro de Projeto</title>
 </head>
 
 <body>
 
 
-    <div class="container">
-        <div clas="span10 offset1">
-          <div class="card">
+<div class="container">
+    <?php
+    // mensagem de erro caso as senhas não sejam iguais
+    if (isset($_GET['erro'])) {
+        echo '<div class="alert alert-danger">As senhas devem ser iguais!</div>';
+    }
+    // mensagem de erro caso o login escolhido já exista no banco de dados
+    if (isset($_GET['repetido'])) {
+        echo '<div class="alert alert-danger">Este Login ou CPF já foi escolhido por outra pessoa!</div>';
+    }
+    // mensagem de sucesso caso o usuario seja cadastrado corretamente
+    if (isset($_GET['successUser'])) {
+        echo '<div class="alert alert-success">Usuario cadastrado!</div>';
+    }
+
+    ?>
+    <div clas="span10 offset1">
+        <div class="card">
             <div class="card-header">
                 <h3 class="well"> Cadastro de Projeto </h3>
             </div>
             <div class="card-body">
-            <form class="form-horizontal" action="#" method="post">
 
-                <div class="control-group <?php echo !empty($nomeProjetoErro)?'error ' : '';?>">
-                    <label class="control-label">Nome do Projeto</label>
-                    <div class="controls">
-                        <input size="50" class="form-control" name="nomeProjeto" type="text" placeholder="Digite o nome do projeto" required="" value="<?php echo !empty($nomeProjeto)?$nomeProjeto: '';?>">
-                        <?php if(!empty($nomeProjetoErro)): ?>
-                            <span class="help-inline"><?php echo $nomeProjetoErro;?></span>
-                            <?php endif;?>
+                <form action="#" method="post">
+                    <div class="control-group">
+                        <label class="control-label">Nome do Projeto</label>
+                        <div class="controls">
+                            <input size="50" class="form-control" name="nomeProjeto" type="text"
+                                   placeholder="Digite o nome do projeto" required=""
+                            >
+                        </div>
                     </div>
-                </div>
 
-                <div class="control-group <?php echo !empty($nomeProfessorErro)?'error ': '';?>">
-                    <label class="control-label">Prof°(a) Orientador</label>
-                    <div class="controls">
-                        <input size="80" class="form-control" name="nomeProfessor" type="text" placeholder="Digite o nome do Orientador" required="" value="<?php echo !empty($nomeProfessor)?$nomeProfessor: '';?>">
-                        <?php if(!empty($nomeProfessorErro)): ?>
-                            <span class="help-inline"><?php echo $nomeProfessorErro;?></span>
-                            <?php endif;?>
+                    <div class="control-group">
+                        <label class="control-label">Prof° Orientador</label>
+                        <div class="controls">
+                            <input size="50" class="form-control" name="nomeProfessor" type="text"
+                                   placeholder="Digite o nome do Orientador" required=""
+                            >
+                        </div>
                     </div>
-                </div>
+            </div>
 
-                <div class="control-group <?php echo !empty($objetivoErro)?'error ': '';?>">
-                    <label class="control-label">Objetivo do Projeto</label>
-                    <div class="controls">
-                        <textarea size="35" class="form-control" name="objetivo" type="text" required="" value="<?php echo !empty($objetivo)?$objetivo: '';?>"> </textarea>
-                        <?php if(!empty($objetivoErro)): ?>
-                            <span class="help-inline"><?php echo $objetivoErro;?></span>
-                            <?php endif;?>
-                    </div>
-                </div>
-
-                <div class="control-group <?php echo !empty($resumoErro)?'error ': '';?>">
-                    <label class="control-label">Resumo do Projeto</label>
-                    <div class="controls">
-                        <textarea size="40" class="form-control" name="resumo" type="text" required="" value="<?php echo !empty($resumo)?$resumo: '';?>"></textarea>
-                        <?php if(!empty($resumoErro)): ?>
-                            <span class="help-inline"><?php echo $resumoErro;?></span>
-                            <?php endif;?>
-                    </div>
-                </div>
-
-
-				<div class="col-sm-3">
-			          <label>Curso</label>
-      <select class="form-control" name="cursProjeto" >
-        <option  value="ti">GESTÃO DA TECNOLOGIA DA INFORMAÇÃO</option>
-        <option  value="contabil">CIÊNCIAS CONTÁBEIS</option>
-		        <option value="adm">ADMINISTRAÇÃO</option>
-				        <option  value="estetica">ESTÉTICA E COSMÉTICA</option>
-						        <option value="mkt">MARKETING</option>
-								        <option value="tpg">TECNOLOGIA EM PROCESSOS GERENCIAIS</option>
-      </select>
-    </div>
-	
-			  	  <div class="col-sm-3">
-			          <span>Turma</span>
-      <select class="form-control" name="turma" <?php echo !empty($turmaErro)?'error ': '';?> >
-        <option value="<?php echo !empty($turma)?$turma: '01';?>">01</option>
-        <option value="<?php echo !empty($turma)?$turma: '02';?>">02</option>
-		        <option value="<?php echo !empty($turma)?$turma: '03';?>">03</option>
-				        <option value="<?php echo !empty($turma)?$turma: '04';?>">04</option>
-						        <option value="<?php echo !empty($turma)?$turma: '05';?>">05</option>
-      </select>
-    </div>
-
-                <div class="form-actions">
-                    <br/>
-
-                    <button type="submit" class="btn btn-success">Adicionar</button>
-                    <a href="index.php" type="btn" class="btn btn-default">Voltar</a>
-
-                </div>
-				
-				<?php
-			echo $_SERVER['REQUEST_METHOD'];
-			
-			  session_start();
-		if(!isset($_SESSION['login']) && !isset($_SESSION['senha'])):
-			header("Location: index.php");	
-				endif;
-								
-			
-			    if(empty($_POST['cadastrar'])) {
-		
-		    include ('../classes/Conexao.class.php');
-			include ('../classes/ProjetoDAO.class.php');
-			
-		$cadastrarProjeto = new ProjetoDAO();
-		
-		$codUsuario = ($_SESSION['codUsuario']) ? $_SESSION['codUsuario'] : '1';
-		$nomeProjeto = ($_POST['nomeProjeto']) ? $_POST['nomeProjeto'] : '1';
-		$nomeProfessor = ($_POST['nomeProfessor']) ? $_POST['nomeProfessor'] : '1';
-		$objetivo = ($_POST['objetivo']) ? $_POST['objetivo'] : '1';
-		$resumo = ($_POST['resumo']) ? $_POST['resumo'] : '1';
-		$cursProjeto = ($_POST['cursProjeto']) ? $_POST['cursProjeto'] : '1';
-		$turma = ($_POST['turma']) ? $_POST['turma'] : '1';
-					  
-      $consultaProjeto = $cadastrarProjeto->unicoProjeto($nomeProjeto);
-      // caso o login escolhido já exista no banco retorna erro
-      if($consultaProjeto == false) {
-        header('location:criar-projeto.php?repetido=senha');
-      // caso não haja login parecido, inclui métoro de inserção de dados no banco de dados
-      } else {
-        $insere = $cadastrarProjeto->cadastraProjeto($codUsuario,$nomeProjeto,$nomeProfessor,$objetivo,$resumo,$cursProjeto,$turma);
-        // caso o usuario seja cadastrado, exibir mensagem de sucesso
-        if($insere == true) {
-          header('location:index.php?successUser=cadastrado');
-        }
-      }
-
-    }
-
-?>
-            </form>
-          </div>
+            <div class="control-group>
+                <label class=" control-label
+            ">Objetivo do Projeto</label>
+            <div class="controls">
+                            <textarea size="35" class="form-control" name="objetivoProjeto" type="text" required=""
+                            > </textarea>
+            </div>
         </div>
+
+        <div class="control-group>
+            <label class="control-label">Resumo do Projeto</label>
+            <div class="controls">
+                            <textarea size="40" class="form-control" name="resumoProjeto" type="text" required=""
+                                     ></textarea>
+            </div>
         </div>
+
+
+        <div class="col-sm-3">
+            <label>Curso</label>
+            <select class="form-control" name="cursoProjeto">
+                <option value="ti">GESTÃO DA TECNOLOGIA DA INFORMAÇÃO</option>
+                <option value="contabil">CIÊNCIAS CONTÁBEIS</option>
+                <option value="adm">ADMINISTRAÇÃO</option>
+                <option value="estetica">ESTÉTICA E COSMÉTICA</option>
+                <option value="mkt">MARKETING</option>
+                <option value="tpg">TECNOLOGIA EM PROCESSOS GERENCIAIS</option>
+            </select>
+        </div>
+
+        <div class="col-sm-3">
+            <label>Turma</label>
+            <select class="form-control" name="turmaProjeto">
+                <option value="01" >01</option>
+                <option value="02" >02</option>
+                <option value="03" >03</option>
+                <option value="04" >04</option>
+                <option value="05" >05</option>
+            </select>
+        </div>
+
+        <div class="form-actions">
+            <br/>
+
+            <button type="submit" class="btn btn-success" name="cadastrar">Adicionar</button>
+            <a href="index.php" type="btn" class="btn btn-default">Voltar</a>
+
+        </div>
+
+        </form>
     </div>
-    </div>
-    <script src="https://code.jquery.com/jquery-3.3.1.js" integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <!-- Latest compiled and minified JavaScript -->
-    <script src="assets/js/bootstrap.min.js"></script>
+</div>
+</div>
+</div>
+</div>
+<script src="https://code.jquery.com/jquery-3.3.1.js" integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60="
+        crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
+        integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+        crossorigin="anonymous"></script>
+<!-- Latest compiled and minified JavaScript -->
+<script src="assets/js/bootstrap.min.js"></script>
 </body>
 
 </html>
--- classes/ProjetoDAO.class.php
+++ classes/ProjetoDAO.class.php
@@ -1,36 +1,40 @@
 <?php
 
-  class ProjetoDAO {
+class ProjetoDAO
+{
     private $conexao;
 
-    public function __construct() {
-      $this->conexao = new Conexao();
+    public function __construct()
+    {
+        $this->conexao = new Conexao();
     }
 
     // cadastra o projeto
-    public function cadastraProjeto($codUsuario,$nomeProjeto,$nomeProfessor,$objetivo,$resumo,$cursProjeto,$turma) {
+    public function cadastra($codUsuario, $nomeProjeto, $nomeProfessor, $objetivoProjeto, $resumoProjeto, $cursoProjeto, $turmaProjeto)
+    {
 
-      $sql = "INSERT INTO tb_projeto (codUsuario,nomeProjeto,nomeProfessor,objetivo,resumo,curso,turma) VALUES ('$codUsuario','$nomeProjeto','$nomeProfessor',$objetivo,'$resumo','$cursProjeto','$turma')";
+        $sql = "INSERT INTO tb_projeto (codUsuario,nomeProjeto,nomeProfessor,objetivo,resumo,curso,turma) VALUES ('$codUsuario','$nomeProjeto','$nomeProfessor','$objetivoProjeto','$resumoProjeto','$cursoProjeto','$turmaProjeto')";
 
-      $executa = mysqli_query($this->conexao->getCon(), $sql);
+        $executa = mysqli_query($this->conexao->getCon(), $sql);
 
-      if(mysqli_affected_rows($this->conexao->getCon()) > 0) {
-        return true;
-      } else {
-        return false;
-      }
+        if (mysqli_affected_rows($this->conexao->getCon()) > 0) {
+            return true;
+        } else {
+            return false;
+        }
     }
-	
-	    public function unicoProjeto($nomeProjeto) {
 
-      $unic = "SELECT * FROM tb_projeto WHERE nomeProjeto = '$nomeProjeto'";
+    public function unicoProjeto($nomeProjeto)
+    {
 
-      $exec = mysqli_query($this->conexao->getCon(), $unic);
+        $unic = "SELECT * FROM tb_projeto WHERE nomeProjeto = '$nomeProjeto'";
 
-      if(mysqli_num_rows($exec) > 0) {
-        return false;
-      } else {
-        return true;
-      }
+        $exec = mysqli_query($this->conexao->getCon(), $unic);
+
+        if (mysqli_num_rows($exec) > 0) {
+            return false;
+        } else {
+            return true;
+        }
     }
-  }
+}
