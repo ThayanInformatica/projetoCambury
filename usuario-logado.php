f81bff3fc964784a89e6a21e1422267625688550
Thayan Alves <thayan_fsa10@hotmail.com>
2019-06-15 14:55:35 -0300

    alteraçoes no menu de icons quebrados

--- usuario-logado.php
+++ usuario-logado.php
@@ -85,6 +85,7 @@ endif;
     <meta name="description"
           content="Responsive sidebar template with sliding effect and dropdown menu based on bootstrap 3">
     <title>Bem-Vindo | Faculdades Cambury</title>
+    <script src="https://kit.fontawesome.com/2a0a6ae899.js"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
           integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
           crossorigin="anonymous">
